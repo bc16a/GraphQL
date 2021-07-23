a307421977d5dfff49c8e1f7fb0d2d92a34e8eda
Bibinson Celissaint <celissaintb@gmail.com>
2021-07-23 00:57:33 -0300

    Requisição assíncrona &  Resolvendo CORS

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,7 +1,11 @@
 
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
+
+const enableCors = cors({origin: 'http://192.168.15.9:3000'});
+
 
 server.get('/status', (_, response)=> {
     response.send({
@@ -9,11 +13,11 @@ server.get('/status', (_, response)=> {
     })
 });
 
-server.post('/authenticate', express.json(), (request, response) => {
+server.options('/authenticate', enableCors ).post('/authenticate', enableCors , express.json(), (request, response) => {
     
     console.log('Email', request.body.email, 'senha',request.body.password)
     
-    response.send();
+    response.send({Okay:true,});
 })
 
 
--- packages/web/src/pages/SignIn.js
+++ packages/web/src/pages/SignIn.js
@@ -1,18 +1,38 @@
-import React from 'react';  
+import React, { useState } from 'react';  
 
 function SignIn() { 
 
+  const [email , setEmail] = useState('')
+
+  const [password , setPassword ] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://192.168.15.9:8000/authenticate', {
+      headers: {
+        'Content-Type':'application/json',
+      },
+      method:'POST',
+      body:JSON.stringify({
+        email,
+        password
+      })
+    }).then((response) => response.json()).then((data)=>{console.log('success!', data)})
+  }
+
+  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
  
   return (
-    <form action="/authenticate" method="POST">
+    <form onSubmit={handleSubmit}>
     <fieldset>
-        <label for="email">E-mail</label>
-        <input id="email" name="email" type="email" autocomplete="username" />
+        <label htmlFor="email">E-mail</label>
+        <input onChange={handleEmailChange} inputMode="email" id="email" type="email" autoComplete="username" value={email}/>
     </fieldset>
 
     <fieldset>
-        <label for="password">Senha</label>
-        <input id="password" name="password" type="password" autocomplete="current-password" />
+        <label htmlFor="password">Senha</label>
+        <input onChange={handlePasswordChange} id="password" type="password" autoComplete="current-password" value={password}/>
     </fieldset>
 
     <button type="submit">Entrar</button>
